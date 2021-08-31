4f1fdf464327f1f13b68f53dee11863dc8dbbf73
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-08-31 20:11:52 +0200

    using larael collective for forms

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -6,30 +6,31 @@
    
 
     <div class="card-body">
-       <form action="{{ url("/products") }}" method="POST" class="form">
-        @csrf
+        {!! Form::open([
+                'url' => '/products', 
+                'method' => 'POST',
+                'class' => 'form']) !!}  
+                {{ Form::token() }}  
             <div class="form-group">
-                <label for="">Name</label>
-                <input type="text" name="product_name" class="form-control" required>
+            
+                {{ Form::label('', 'Name') }}
+                {{ Form::text('product_name', '', ['class' => 'form-control']) }}
             </div>
 
             <div class="form-group">
-                <label for="">Price</label>
-                <input type="text" name="product_price" class="form-control" required>
+                {{ Form::label('', 'Price') }}
+                {{ Form::text('product_price', '', ['class' => 'form-control']) }}
             </div>
 
             <div class="form-group">
-                <label for="">Description</label>
-                <textarea class="form-control" 
-                name="product_description" id="" cols="30" rows="10" required></textarea>
+                {{ Form::label('', 'Description') }}
+                {{ Form::textarea('product_description', '', ['class' => 'form-control']) }}
             </div>
 
             <div class="form-group">
-                <input type="submit" value="Save" class="btn btn-info">
+               {{ Form::submit('Save Product', ['class' => 'btn btn-info']) }}
             </div>
 
-        
-        </form>
     </div>
 @endsection
 
