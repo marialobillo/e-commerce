e7edd16da0af063a7ecb34d0f9d87aa0deb0ee31
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-09-19 20:19:56 +0200

    refactoring show category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -56,10 +56,8 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Category $category)
     {
-        $category = Category::findOrFail($id);
-
         return view('categories.show', compact('category'));
     }
 
