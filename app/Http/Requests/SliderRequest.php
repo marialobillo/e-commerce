72b6c0824eb553bbdf9bb68df5c19a7f0f3409cd
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-09-26 18:37:51 +0200

    setting up request for sliders

---
+++ app/Http/Requests/SliderRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SliderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'description1' => 'required',
+            'description2' => 'required',
+            'slider_image' => 'required'
+        ];
+    }
+}
