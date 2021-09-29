ac14a3cdf205e7c693ca338e9ebf2457be4dd501
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-09-29 19:47:03 +0200

    create view for sliders

--- resources/views/admin/sliders/create.blade.php
+++ resources/views/admin/sliders/create.blade.php
@@ -17,82 +17,55 @@
                 </div>
                 <!-- /.card-header -->
                 <!-- form start -->
-                <form >
-                  <div class="card-body">
-                    <div class="form-group">
-                      <label for="exampleInputEmail1">Slider description 1</label>
-                      <input type="text" name="description1" class="form-control" id="exampleInputEmail1" placeholder="Enter slider description">
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleInputEmail1">Slider description 2</label>
-                      <input type="text" name="description2" class="form-control" id="exampleInputEmail1" placeholder="Enter slider description">
-                    </div>
-                    <label for="exampleInputFile">Slider image</label>
-                    <div class="input-group">
-                      <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="exampleInputFile">
-                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                      </div>
-                      <div class="input-group-append">
-                        <span class="input-group-text">Upload</span>
-                      </div>
-                    </div>
-                  </div>
-                  <!-- /.card-body -->
-                  <div class="card-footer">
-                    <!-- <button type="submit" class="btn btn-warning">Submit</button> -->
-                    <input type="submit" class="btn btn-warning" value="Save" >
-                  </div>
-                </form>
+                   <!-- form start -->
+                   {!! Form::open([
+                    'url' => '/sliders', 
+                    'method' => 'POST',
+                    'class' => 'form',
+                    'files' => 'true',
+                    ]) !!}  
+                    {{ Form::token() }} 
 
 
-                 <!-- form start -->
-                 {!! Form::open([
-                  'url' => '/products', 
-                  'method' => 'POST',
-                  'class' => 'form',
-                  'files' => 'true',
-                  ]) !!}  
-                  {{ Form::token() }} 
-                  
-                    
-                    <div class="card-body">
-                      <div class="form-group">
-                        {{ Form::label('', 'Name') }}
-                        {{ Form::text('product_name', '', ['class' => 'form-control']) }}
-                      </div>
-      
-                      <div class="form-group">
-                          {{ Form::label('', 'Price') }}
-                          {{ Form::text('product_price', '', ['class' => 'form-control']) }}
-                      </div>
-      
-                      <div class="form-group">
-                          {{ Form::label('', 'Image') }}
-                          {!! Form::file('product_image', ['class' => 'form-control']) !!}
-                      </div>
-      
-                      <div class="form-group">
+
+                      <div class="card-body">
+                        
+                        <div class="form-group">
+                          {{ Form::label('', 'Description 1') }}
+                          {{ Form::text('description1', '', ['class' => 'form-control']) }}
+                        </div>
+        
+                        <div class="form-group">
+                            {{ Form::label('', 'Description 2') }}
+                            {{ Form::text('description2', '', ['class' => 'form-control']) }}
+                        </div>
+        
+                        <div class="form-group">
+                            {{ Form::label('', 'Image') }}
+                            {!! Form::file('slider_image', ['class' => 'form-control']) !!}
+                        </div>
+
+                        <div class="form-group">
                           {{ Form::label('', 'Status') }}
                           {!! Form::number('status',null,['class' => 'form-control']) !!}
                       </div>
-      
-                      <div class="form-group">
-                          {{ Form::label('', 'Category') }}
-                          {{ Form::select('category_id', $categories, null, 
-                            [
-                              'class' => 'form-control', 
-                              'placeholder' => 'Select Category'
-                            ]) }}
+        
                       </div>
-                      
-                    </div>
                    
       
-                    <div class="card-footer">
-                      {{ Form::submit('Save Product', ['class' => 'btn btn-info']) }}
-                    </div>
-                {!! Form::close() !!}
+                      <div class="card-footer">
+                        {{ Form::submit('Save Slider', ['class' => 'btn btn-info']) }}
+                      </div>
+                    {!! Form::close() !!}
+
+
+                
+
+
+              
+                  
+                    
+                  
 
                 
               </div>
